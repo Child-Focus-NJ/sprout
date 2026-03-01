bff4f9fb6a1677171e077b0d82c1d06f4acb1242
Isabelle Larson <ivlarson20@gmail.com>
2026-03-01 17:11:01 -0500

    BDD for user storeis 1-4, added feature specs and step def to a failing stage

--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -1,10 +1,9 @@
 Given("I am a signed-in system administrator") do
-  @user = User.create!(
-    email: "admin@example.com",
-    first_name: "Admin",
-    last_name: "User",
-    role: :admin
-  )
+  @user = User.find_or_create_by!(email: "admin@example.com") do |u|
+    u.first_name = "Admin"
+    u.last_name  = "User"
+    u.role       = :admin
+  end
+
   visit root_path
-  expect(page).to have_content("Admin Dashboard")
 end
