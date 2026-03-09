daab34765061d219febedb098fa59c46c3f6f834
sufyanw <sufyanbwaryah@gmail.com>
2026-03-09 01:08:24 -0400

    Fix failing lint test, errors for EmptyLinesAroundBlockBody, SpaceInsideArrayLiteralBrackets

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,7 +1,7 @@
 require "warden"
 
 Rails.application.config.middleware.use Warden::Manager do |manager|
-  manager.failure_app = ->(env) { [302, { "Location" => "/" }, []] }
+  manager.failure_app = ->(env) { [ 302, { "Location" => "/" }, [] ] }
 end
 
 Warden::Manager.serialize_into_session(:user) { |user| user.id }
