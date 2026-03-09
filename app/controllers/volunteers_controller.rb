2a9b16e317d88a2a90ece644d63055f47969dc0b
sufyanw <sufyanbwaryah@gmail.com>
2026-03-08 23:35:44 -0400

    Add routes and controllers for volunteer status management and session check-in

---
+++ app/controllers/volunteers_controller.rb
@@ -0,0 +1,33 @@
+class VolunteersController < ApplicationController
+  before_action :set_volunteer, only: [ :show, :update_status, :send_application, :mark_submitted ]
+
+  def show
+  end
+
+  def update_status
+    @volunteer.change_status!(params[:status], user: current_user)
+    redirect_to volunteer_path(@volunteer)
+  end
+
+  def send_application
+    if @volunteer.application_sent_at.present?
+      redirect_to volunteer_path(@volunteer), alert: "Application was already sent"
+    else
+      @volunteer.change_status!(:application_sent, user: current_user)
+      @volunteer.update!(application_sent_at: Time.current)
+      redirect_to volunteer_path(@volunteer), notice: "Application email queued for #{@volunteer.full_name}"
+    end
+  end
+
+  def mark_submitted
+    @volunteer.change_status!(:applied, user: current_user)
+    @volunteer.update!(application_submitted_at: Time.current)
+    redirect_to volunteer_path(@volunteer)
+  end
+
+  private
+
+  def set_volunteer
+    @volunteer = Volunteer.find(params[:id])
+  end
+end
