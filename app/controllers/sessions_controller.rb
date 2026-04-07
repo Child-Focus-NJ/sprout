75feb50f8a034afd5f148956224ad74698e50128
sufyanw <sufyanbwaryah@gmail.com>
2026-04-06 22:41:21 -0400

    persist user display name and oauth avatar metadata

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,11 +13,22 @@ class SessionsController < ApplicationController
     email = auth.info.email
 
     if email && (email.end_with?("@childfocusnj.org") || email.end_with?("@nyu.edu")) # added nyu for testing
+      name_parts = auth.info.name.to_s.split
+      first_from_name = name_parts.first
+      last_from_name = name_parts.length > 1 ? name_parts[1..].join(" ") : nil
+
       user = User.find_or_create_by(google_uid: auth.uid) do |u|
         u.email = email
-        u.first_name = auth.info.first_name || auth.info.name.split.first
-        u.last_name  = auth.info.last_name || auth.info.name.split.last
+        u.first_name = auth.info.first_name.presence || first_from_name
+        u.last_name = auth.info.last_name.presence || last_from_name
+        u.avatar_url = auth.info.image.presence
       end
+
+      user.update!(
+        first_name: auth.info.first_name.presence || first_from_name || user.first_name,
+        last_name: auth.info.last_name.presence || last_from_name || user.last_name,
+        avatar_url: auth.info.image.presence || user.avatar_url
+      )
 
       session[:user_id] = user.id
       redirect_to volunteers_path
